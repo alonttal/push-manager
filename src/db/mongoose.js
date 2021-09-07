a49d39dde1885228fb13fa0c849acd8c54d651f6
Alon Talmor <alontalmor@gmail.com>
2021-09-07 08:53:05 +0300

    Improve mongoose connect code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL)
-.catch(e => console.log(e))
+connect()
+
+async function connect() {
+	try {
+		await mongoose.connect(process.env.MONGODB_URL)
+		console.log('connected to db')
+	} catch (e) {
+		console.log(e)
+	}
+}
